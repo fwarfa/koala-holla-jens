1f45be211d555942574c33375bd3adc7ce85a082
Dan St. Aubin <staubindj@gmail.com>
2021-08-05 13:15:30 -0500

    added db configuration under //DB CONNECTION

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,7 +2,14 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+const pg = require('pg');
+pool = new pg.Pool({
+    database: "koala",
+    host: 'localhost',
+    port: 5432,
+    max: 20,
+    idleTimeoutMillis: 4500
+})
 
 // GET
 
