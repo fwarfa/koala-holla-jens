9fe0a3793e1029020c5da38fd53c2b7fa4d115f7
samson vang <vangsam2@gmail.com>
2021-08-05 13:29:29 -0500

    Koala router.GET

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pg = require('pg');
 
 // DB CONNECTION
 
 
 // GET
+router.get('/', (res, res) => {
+    let sqlQuery = `
+        SELECT * FROM "koala"
+    `;
+    pool.query(sqlQuery)
+    .then((dbRes) => {
+        console.log(dbRes.rows);
+        res.send(dbRes.rows)
+    }).catch((err) => {
+        console.log('SQL failed', err)
+        res.sendStatus(500);
+    })
+});
 
 
 // POST
